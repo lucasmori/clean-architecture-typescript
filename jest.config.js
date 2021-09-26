dfe0c497a68778245dc1dae674bf06cb989d4ffb
Lucas Mori <lucastecnew@gmail.com>
2021-09-26 17:04:15 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- jest.config.js
+++ jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   testEnvironment: 'node',
-  preset: ' @shelf/jest-mongodb',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
